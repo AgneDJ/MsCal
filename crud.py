6950cde53bc836d7cd5604650d6b1d99b77bc721
AgneDJ <fizikee@gmail.com>
2024-03-04 17:58:38 -0800

    Added crud.py skeleton

---
+++ crud.py
@@ -0,0 +1,17 @@
+"""CRUD operations."""
+
+from model import db, User, connect_to_db
+from sqlalchemy import insert, or_, and_
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import func
+import os
+from datetime import date, datetime
+
+
+def create_user(name, email):
+    """Create and return a new user."""
+
+    user = User(user_name=name, user_email=email,
+                user_password="", user_avatar="")
+
+    return user
